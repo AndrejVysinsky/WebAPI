a9851af4e42f37ac9a38daa7efef20549c11a1d7
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2024-03-10 14:07:43 +0100

    default to swagger on released

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -42,10 +42,18 @@ namespace WebAPI
 
             var app = builder.Build();
 
+            app.UseSwagger();
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
                 app.UseSwaggerUI();
+            }
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
