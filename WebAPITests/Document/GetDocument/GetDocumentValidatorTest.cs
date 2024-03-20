de56121d105e4517e023f2626c27b5aa0b4abe66
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2024-03-20 13:23:19 +0100

    fixed folder structure

--- WebAPITests/Document/GetDocument/GetDocumentValidatorTest.cs
+++ WebAPITests/Document/GetDocument/GetDocumentValidatorTest.cs
@@ -2,29 +2,69 @@
 using Moq;
 using WebAPI.Handlers.Document.GetDocument;
 using WebAPI.Repositories;
-using Xunit;
 
 namespace WebAPITests.Document.GetDocument
 {
     public class GetDocumentValidatorTest
     {
         [Fact]
-        public void Validate_WithValidDocument_ShouldNotHaveValidationError()
+        public void GetDocument_WhenEmptyId_ShouldReturnValidationError()
         {
             //Arrange
-            var request = new GetDocumentRequest() { Id = 1 };
-            var document = new WebAPI.Data.Document { Id = 1, Tags = "tag1,tag2", Data = "Some data" };
-
-            var documentRepositoryMock = new Mock<IDocumentRepository>();
-            documentRepositoryMock.Setup(repo => repo.GetDocument(It.IsAny<int>())).ReturnsAsync(document);
-
-            var validator = new GetDocumentRequestValidator(documentRepositoryMock.Object);
+            var arrangments = new Arrangments(0);
 
             //Act
-            var result = validator.TestValidate(request);
+            var result = arrangments.Validator.TestValidate(arrangments.Request);
+
+            //Assert
+            result.ShouldHaveValidationErrorFor("Id");
+        }
+
+        [Fact]
+        public void GetDocument_WhenNotExistingId_ShouldReturnValidationError()
+        {
+            //Arrange
+            var arrangments = new Arrangments(1, idExists: false);
+
+            //Act
+            var result = arrangments.Validator.TestValidate(arrangments.Request);
+
+            //Assert
+            result.ShouldHaveValidationErrorFor("Id");
+        }
+
+        [Fact]
+        public void GetDocument_WhenValidId_ShouldSucceed()
+        {
+            //Arrange
+            var arrangments = new Arrangments(1, idExists: true);
+
+            //Act
+            var result = arrangments.Validator.TestValidate(arrangments.Request);
 
             //Assert
             result.ShouldNotHaveAnyValidationErrors();
+        }
+                
+        private class Arrangments
+        {
+            public GetDocumentRequest Request { get; private set; }
+            public Mock<IDocumentRepository> DocumentRepoMock { get; private set; }
+            public GetDocumentRequestValidator Validator { get; private set; }
+
+            public Arrangments(int documentId, bool idExists = false)
+            {
+                Request = new GetDocumentRequest()
+                {
+                    Id = documentId
+                };
+
+                DocumentRepoMock = new Mock<IDocumentRepository>();
+                DocumentRepoMock.Setup(x => x.GetDocument(It.IsAny<int>()))
+                    .ReturnsAsync(idExists ? new WebAPI.Data.Document() : null);
+
+                Validator = new GetDocumentRequestValidator(DocumentRepoMock.Object);
+            }
         }
     }
 }
